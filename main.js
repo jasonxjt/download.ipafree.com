a8e605ff90f199d8de1ec206e9608aaf1de56f7b
JasonXJT <105022890+jasonxjt@users.noreply.github.com>
2022-05-11 17:02:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var isApp = "standalone" in window.navigator && window.navigator.standalone;
+    var isApp = true;
+    var eHome = document.getElementById("home");
+    var eApp = document.getElementById("app");
+
+    if (isApp) {
+        eHome.style.display = "none";
+        eApp.style.display = "block";
+    }
+}
